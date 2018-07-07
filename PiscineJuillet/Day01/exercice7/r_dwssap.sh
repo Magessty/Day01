329bbd9276c3e7e71fde902012608f93a4ec0820
Robin Duquenne <dduquenne62730@hotmail.fr>
2018-07-07 21:16:29 +0200

    exercice 7

--- PiscineJuillet/Day01/exercice7/r_dwssap.sh
+++ PiscineJuillet/Day01/exercice7/r_dwssap.sh
@@ -1,3 +1,1 @@
-#!/bin/sh
-
-cat  /etc/passwd | grep -v '#' | sed "1 d;n;d" | cut -d : -f 1,10 | rev | sort -r | awk 'NR > 0 {for (n="$FT_LINE1";o="FT_LINE2";n<=NR;o>=NR) {print $NF}}'
+echo `cat /etc/passwd | grep -v '#' | sed "1 d;n;d" | cut -d : -f 1 | rev | sort -r | awk "NR>=$FT_LINE1 && NR<=$FT_LINE2"` | tr '\n' '.'  | sed -e 's/ /, /g'
