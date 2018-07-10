913bd20107d1ff05a2588b4ec7ee586f63ac1551
Robin Duquenne <dduquenne62730@hotmail.fr>
2018-07-10 19:34:51 +0200

    sauvegarde

---
+++ PiscineJuillet/Day04/exercice4/ft_fibonacci.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rduquenn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/10 17:46:08 by rduquenn          #+#    #+#             */
+/*   Updated: 2018/07/10 17:54:52 by rduquenn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_fibonacci(int index)
+{
+	if (index < 1)
+		return (-1);
+	else if (index == 0)
+		return (0);
+	else if (index == 1 || index == 2)
+		return (1);
+	else
+	{
+		if (index >= 2)
+			return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+	}
+}
